692553230ce129fc6965af0e002e0507f6d203af
david toska <david.toska@outlook.com>
2020-02-11 00:02:30 +0100

    updated snap

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
-const http = require('http');
-const express = require('express');
+const express = require("express");
 
-const hostname = '0.0.0.0';
 const port = process.env.NODE_PORT || 3007;
 const env = process.env;
 
 const expressServer = express();
-expressServer.get('/', (req, res) => {
+expressServer.get("/", (req, res) => {
   let env_vars = [];
   for (let k in env) {
-    env_vars.push(`${k}: ${env[k]} \n`)
+    env_vars.push(`${k}: ${env[k]} \n`);
   }
   env_vars.push("VERSION: EXPRESS-SERVICE1");
   return res.send(env_vars);
-})
+});
 
-expressServer.listen(port, () => console.log(`Example app listening on port ${port}!`))
+expressServer.get("/test", (req, res) => {
+  return res.send("Updated service. Wonder if it gets deployed!!");
+});
 
+expressServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 /**
 const server = http.createServer((req, res) => {
